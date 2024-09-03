14cd055f43ab57d1ff171533705446e3965d9066
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-04 00:04:42 +0300

    New files structure in the project.

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,18 @@
+export default class LinkedList {
+
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        //TODO
+    }
+
+}
+
+
