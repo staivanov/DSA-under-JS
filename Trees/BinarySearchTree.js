b5543c6532c6dc8c3ffe3d6cc3a3978461948676
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-22 20:21:28 +0300

    Node class was created.

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,17 @@
+import { Node } from "../Trees/node";
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+
+    }
+
+    lookup(value) {
+
+    }
+
+
+}
--- Trees/main.js
+++ Trees/main.js
@@ -1,6 +1,13 @@
-function BinaryTreeNode(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-}
+//   9
+ // 4  20
+// 1 6 15 170
+
+const tree = new BInarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6)
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
 
